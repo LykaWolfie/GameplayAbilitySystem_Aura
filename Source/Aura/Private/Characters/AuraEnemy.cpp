9deda0a7eddf95469682de73b6e2de601c0a32b1
Andrei Fernandez <andrei.fernandez@secret6.com>
2024-02-12 20:51:40 +0800

    Post Process Highlights (Chapter 1 done! Good job!)

--- Source/Aura/Private/Characters/AuraEnemy.cpp
+++ Source/Aura/Private/Characters/AuraEnemy.cpp
@@ -3,12 +3,19 @@
 
 #include "Characters/AuraEnemy.h"
 
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
+
 void AAuraEnemy::HighlightActor()
 {
-	IsHighlighted = true;
+	GetMesh()->SetRenderCustomDepth(true);
+	Weapon->SetRenderCustomDepth(true);
 }
 
 void AAuraEnemy::UnhighlightActor()
 {
-	IsHighlighted = false;
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
 }
