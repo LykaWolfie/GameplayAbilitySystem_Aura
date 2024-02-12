999715e1a84a2af3e9ec0cf95b9f39337bafea11
Andrei Fernandez <andrei.fernandez@secret6.com>
2024-02-12 14:20:27 +0800

    Player and enemy characters

---
+++ Source/Aura/Private/Characters/AuraCharacterBase.cpp
@@ -0,0 +1,17 @@
+
+
+
+#include "Characters/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
